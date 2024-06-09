0e7cd6e1221c219d36f76a6fb8f764673029039f
rujool11 <132924872+rujool11@users.noreply.github.com>
2024-06-09 17:06:10 +0530

    use vite instead of create-react-app

---
+++ frontend/src/main.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { ChakraProvider } from '@chakra-ui/react'
+
+const rootElement = document.getElementById('root')
+ReactDOM.createRoot(rootElement).render(
+  <React.StrictMode>
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
+  </React.StrictMode>,
+)
