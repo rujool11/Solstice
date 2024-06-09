a7a33e6d61962a764e41040097a3b75772f75a46
rujool11 <132924872+rujool11@users.noreply.github.com>
2024-06-09 12:12:15 +0530

    add .env to hide port

---
+++ backend/data/data.js
@@ -0,0 +1,109 @@
+export const chats = [
+    {
+      isGroupChat: false,
+      users: [
+        {
+          name: "Alice Smith",
+          email: "alice@example.com",
+        },
+        {
+          name: "Bob Johnson",
+          email: "bob@example.com",
+        },
+      ],
+      _id: "123a456e78b90123c456d789",
+      chatName: "Alice Smith",
+    },
+    {
+      isGroupChat: false,
+      users: [
+        {
+          name: "Charlie Brown",
+          email: "charlie@example.com",
+        },
+        {
+          name: "Bob Johnson",
+          email: "bob@example.com",
+        },
+      ],
+      _id: "123a456e78b90123c456d790",
+      chatName: "Charlie Brown",
+    },
+    {
+      isGroupChat: false,
+      users: [
+        {
+          name: "David Green",
+          email: "david@example.com",
+        },
+        {
+          name: "Bob Johnson",
+          email: "bob@example.com",
+        },
+      ],
+      _id: "123a456e78b90123c456d791",
+      chatName: "David Green",
+    },
+    {
+      isGroupChat: true,
+      users: [
+        {
+          name: "Alice Smith",
+          email: "alice@example.com",
+        },
+        {
+          name: "Bob Johnson",
+          email: "bob@example.com",
+        },
+        {
+          name: "Charlie Brown",
+          email: "charlie@example.com",
+        },
+      ],
+      _id: "123a456e78b90123c456d792",
+      chatName: "Study Group",
+      groupAdmin: {
+        name: "Charlie Brown",
+        email: "charlie@example.com",
+      },
+    },
+    {
+      isGroupChat: false,
+      users: [
+        {
+          name: "Eve White",
+          email: "eve@example.com",
+        },
+        {
+          name: "Bob Johnson",
+          email: "bob@example.com",
+        },
+      ],
+      _id: "123a456e78b90123c456d793",
+      chatName: "Eve White",
+    },
+    {
+      isGroupChat: true,
+      users: [
+        {
+          name: "Alice Smith",
+          email: "alice@example.com",
+        },
+        {
+          name: "Bob Johnson",
+          email: "bob@example.com",
+        },
+        {
+          name: "Charlie Brown",
+          email: "charlie@example.com",
+        },
+      ],
+      _id: "123a456e78b90123c456d794",
+      chatName: "Project Team",
+      groupAdmin: {
+        name: "Charlie Brown",
+        email: "charlie@example.com",
+      },
+    },
+  ];
+  
--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,32 @@
 import express from "express";
+import { chats } from "./data/data.js";
+import { dotenv } from "dotenv";
 
 const app = express();
 
 // Route handler for HTTP GET for root of app /
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
     res.send("API is running");
 });
 
+
+// /api/chat get, send all dummy chats
+app.get("/api/chat", (req, res) => {
+    res.send(chats);
+});
+
+// when id is specified, log id and search chats object for chat whose id matches requested id, 
+// and send it in resonse
+// .find(x) , where x is a function which determines if find returns true based on function evaluation
+app.get("/api/chat/:id", (req, res) => {
+    console.log(req.params.id);
+    const oneChat = chats.find(c => c._id === req.params.id);
+    res.send(oneChat);
+});
+
+const PORT = process.env.PORT;
+// to hide actual port number, put it in .env and use dotenv package
+
 app.listen(5000, () => {
-    console.log("server started at port 5000");
+    console.log(`Server started on port ${PORT}`);
 });
