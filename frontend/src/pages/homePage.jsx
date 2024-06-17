d1153a9b59a16c2f1e3f913de3fb0c153b9bb06c
rujool11 <132924872+rujool11@users.noreply.github.com>
2024-06-17 12:02:21 +0530

    change backgrond, start form

--- frontend/src/pages/homePage.jsx
+++ frontend/src/pages/homePage.jsx
@@ -1,11 +1,26 @@
 import React from 'react'
+import { Container, Box, Text } from "@chakra-ui/react"
 
 const homePage = () => {
   return (
-    <div>
-    Home Page
-    </div>
-  )
+  <Container maxW="xl" centerContent>
+    <Box
+      display="flex"
+      justifyContent="center"
+      padding={3}
+      bg={"white"}
+      width="100%"
+      m="40px 0 15px 0"
+      borderRadius="0.5rem"
+      borderWidth="1px"
+    >
+      <Text fontSize="2xl" fontFamily={"Kanit"} fontWeight={"300"}>WeChat</Text>
+    </Box>
+
+    <Box bg={"white"} padding={3} borderRadius="0.5rem" width="100%" borderWidth="1px"></Box>
+
+  </Container>
+  );
 }
 
 export default homePage
