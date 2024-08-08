9d25fa2d27c00e665f2c26964818fb7642be0ad0
rujool11 <132924872+rujool11@users.noreply.github.com>
2024-08-08 14:28:15 +0530

    change background

---
+++ frontend/src/context/ChatProvider.js
@@ -0,0 +1,10 @@
+import {createContext } from 'react';
+
+const chatContext  = createContext();
+
+const ChatProvider = ({children}) => {
+    return(<chatContext.Provider>{children}</chatContext.Provider>);
+};
+
+export default ChatProvider;
+
