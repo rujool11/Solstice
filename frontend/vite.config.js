43a5ea75587bb513662226bd56bb04dadc0c8b67
rujool11 <132924872+rujool11@users.noreply.github.com>
2025-01-25 22:09:57 +0530

    Add containerization for frontend and backend, i.e add Dockerfiles for frontend and backend and orchestrate services using docker-compose in root

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://127.0.0.1:5000',
+        target: 'http://backend:5000', // changed to //backend for docker, else //locallost/ 172.0.0.1
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, 'api')
       }
