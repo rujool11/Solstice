d414b8de0860ab932c9b1bb5cab86b79eab567c5
rujool11 <132924872+rujool11@users.noreply.github.com>
2025-01-28 14:52:35 +0530

    restore to pre docker

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://backend:5000', // changed to //backend for docker, else //locallost/ 172.0.0.1
+        target: 'http://127.0.0.1:5000',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, 'api')
       }
